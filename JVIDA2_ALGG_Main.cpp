62499597805a7e240ce23eb22c2a0dfdd27dfaed
Hélio Masaru Sueta <136609324+HMasaru@users.noreply.github.com>
2024-09-20 02:59:44 -0300

    Add files via upload

---
+++ JVIDA2_ALGG_Main.cpp
@@ -0,0 +1,16 @@
+/* JVIDA_ALGG_Main.cpp
+JVIDA_ALGG - Projeto Jogo da vida
+22/11/2023
+*/
+#include "JVIDA2_ALGG_Model.cpp"
+#include "JVIDA2_ALGG_Controller.cpp"
+
+
+int main (){
+	tamanhoMundo();
+	iniciaListas();
+	while (sair == 1){
+	menuPrincipal();
+	}
+}
+
